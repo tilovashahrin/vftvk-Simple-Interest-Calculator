96f57403f51220e0aa7ec27b30f3bd9050f3e324
tilovashahrin <tilova.shahrin@ontariotechu.net>
2021-10-10 18:43:51 -0400

    check if principal is positive

--- script.js
+++ script.js
@@ -6,11 +6,16 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
 
-    document.getElementById("result").innerHTML = 
+    if(principal > 0){
+        document.getElementById("result").innerHTML = 
     "\<br\>If you deposit "+ "<mark>"+principal+"</mark>"+
     "\<br\>at an interest rate of "+ "<mark>"+rate+"</mark>"+
     "\<br\>You will receive an amount of "+ "<mark>"+interest+"</mark>"+
     "\<br\>in the year "+ "<mark>"+year+"</mark>";
+    } else{
+        alert("Please enter a positive principal amount.");
+    }
+    
     
 }
 
